e7324c61029c7ac4933464a7862ea4a3992a3537
aose-yuu <aose.developer@gmail.com>
2023-11-24 21:17:14 +0900

    feat: init files

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+// vite.config.ts
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: './src/providers/index.ts',
+      name: 'lazy-provide-inject',
+      fileName: (format) => `lazy-provide-inject.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
