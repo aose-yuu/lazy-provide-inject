e7aff6c5e4a13ceb72a8ec68f83a98c1cceed83a
aose-yuu <aose.developer@gmail.com>
2023-11-25 04:30:16 +0900

    fix: change the build settings in npm packages

--- src/App.vue
+++
@@ -1,6 +0,0 @@
-// App.vue
-<template>
-  <div id="app">
-    <router-view />
-  </div>
-</template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@
 
 import { resolve } from 'path';
 
-// import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [vue()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
