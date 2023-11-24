6e7863268b7088bdc36b5c979a105592f14eecb3
aose-yuu <aose.developer@gmail.com>
2023-11-24 21:21:05 +0900

    feat: export and import path file

---
+++ src/providers/index.ts
@@ -0,0 +1,4 @@
+export { createProvider } from './createProvider';
+export { lazyProvide } from './lazyProvide';
+export { lazyInject } from './lazyInject';
+export type { Provider } from './type.d.ts';
