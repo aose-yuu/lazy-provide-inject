5101544c9a775ef2f144a3fe66763c01dbfceb24
aose-yuu <aose.developer@gmail.com>
2023-11-24 21:21:58 +0900

    feat: GrandChild and TheChild component

---
+++ src/components/TheChild.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { lazyInject } from '../providers';
+import { counterProvider } from '../providers/counterProvider';
+import { usersProvider } from '../providers/usersProvider';
+
+import GrandChild from './GrandChild.vue';
+
+const { count } = lazyInject(counterProvider);
+const { users } = lazyInject(usersProvider);
+</script>
+
+<template>
+  <div class="child-component">
+    <h1>Child</h1>
+    <p>injected 'count' from counterProvider</p>
+    <p>count is: {{ count }}</p>
+    <p>injected 'users' from usersProvider</p>
+    <p>users are: {{ users }}</p>
+    <GrandChild />
+  </div>
+</template>
+
+<style scoped>
+.child-component {
+  padding: 3rem;
+  border: 1px solid red;
+}
+</style>
