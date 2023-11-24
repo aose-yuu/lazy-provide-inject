7f124122f5eb1fbbbf0a1839df1d7d2f38c7a1a6
aose-yuu <aose.developer@gmail.com>
2023-11-25 04:00:23 +0900

    fix: change the build settings in npm packages

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+// App.vue
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { resolve } from 'path';
 
 // import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +14,13 @@ export default defineConfig({
       name: 'lazy-provide-inject',
       fileName: (format) => `lazy-provide-inject.${format}.js`,
     },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   },
-  plugins: [dts()],
 });
